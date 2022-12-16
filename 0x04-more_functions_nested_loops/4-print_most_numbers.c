1e912015aecce6ab135452a52a5bbc8fa8f45efd
Chibuikelawrence <chibuikenwafor91@yahoo.com>
2022-12-16 06:05:17 -0800

    task 5

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_most_numbers - print num except 2 and 4
  *
@@ -12,7 +13,7 @@ void print_most_numbers(void)
 	for (c = '0'; c <= '9'; c++)
 	{
 		if (!(c == '2' || c == '4'))
-			print (c);
+			_putchar(c);
 	}
-	print('\n');
+	_putchar('\n');
 }
