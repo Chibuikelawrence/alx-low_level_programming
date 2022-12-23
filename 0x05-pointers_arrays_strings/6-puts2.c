04152e1f38055ce4a9c4cf0f2073351af2e155f3
Chibuikelawrence <chibuikenwafor91@yahoo.com>
2022-12-22 22:06:57 -0800

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,9 +1,7 @@
 #include "main.h"
 /**
- * puts2 - print every other character of a string
- *
+ * puts2 - prints every other character of a string
  * @str: char to check
- *
  * Return: 0 is success
  */
 
